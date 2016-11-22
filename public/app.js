3c7a284a8fe2cfae38a858b89b49c114b20a747c
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-21 22:58:03 -0800

    Update initial state

--- public/app.js
+++ public/app.js
@@ -19,7 +19,27 @@ function reducer(state, action) {
   }
 }
 
-const initialState = { messages: [] };
+const initialState = {
+  activeThread: '1-fca2',
+  threads: [
+    {
+      id: '1-fca2',
+      title: 'Buzz Aldrin'
+      messages: [
+        {
+          text: 'Twelve minutes to ignition.',
+          timestamp: Date.now(),
+          id: uuid.v4()
+        }
+      ]
+    },
+    {
+      id: '2-be91',
+      title: 'Michael Collins',
+      messages: []
+    }
+  ]
+};
 
 const store = Redux.createStore(reducer, initialState);
 
